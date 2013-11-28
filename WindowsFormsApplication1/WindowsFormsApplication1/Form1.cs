b40f77a21ce9b208ba45999dbe1ea216bab578af
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-28 15:10:08 +0200

    Deneme1

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace AnaOkuluBilisim
+{
+    public partial class ChangePassword : Form
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=.; database=AnaOkuluDB;integrated security=true");
+        SqlCommand cmd;
+        string qry;
+    
+        private void btncreatepwd_Click_1(object sender, EventArgs e)
