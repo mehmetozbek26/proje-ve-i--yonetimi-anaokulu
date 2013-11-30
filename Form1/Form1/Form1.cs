85eb0b09044d5971e436d59147b14dbc9fce577c
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-30 22:20:58 +0200

    Form1

--- Form1/Form1/Form1.cs
+++ Form1/Form1/Form1.cs
@@ -47,7 +47,37 @@ namespace AnaOkuluBilisim
                 cnn.Open();
                 SqlCommand cmd = new SqlCommand("select userid,confirmpassword,comdepart from pwd where userid='" + txtuserid.Text + "' and confirmpassword='" + txtpwd.Text + "' and comdepart='" + comboBox1.SelectedItem + "'", cnn);
 
-               
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    if (txtuserid.Text.ToString() == rdr["userid"].ToString() && txtpwd.Text.ToString() == rdr["confirmpassword"].ToString())
+                    {
+                        //MessageBox.Show("Giriş başarılı.");
+                    }
+                    GlobalClass.GlobalVar = rdr["comdepart"].ToString();
+                    Form yeniform = new ListeForm();
+
+
+
+                    yeniform.Size = Screen.PrimaryScreen.WorkingArea.Size;
+                    yeniform.Show();
+                    this.Hide();
+                    rdr.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı kullanıcı adı veya şifre.");
+                }
+                cnn.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
 
 
     }
