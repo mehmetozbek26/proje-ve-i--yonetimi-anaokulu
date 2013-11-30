38e5f08a2c0ba046557b5581bddbd98e39f1b84d
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-30 21:50:51 +0200

    Form1

---
+++ Form1/Form1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AnaOkuluBilisim
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            txtpwd.Text = "niit";
+            comboBox1.SelectedIndex = 1;
+            txtuserid.Text = "mahmut";
+
+
+
+        }
+        SqlConnection cnn = new SqlConnection("Data Source=.; database=AnaOkuluDB;integrated security=true");
+
+       
+
+
+
+    }
+}
