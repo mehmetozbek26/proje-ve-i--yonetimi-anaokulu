c0d65f1f1991d263219044d22849e6f57f834831
sedatakyol-49 <sedatakyol92@gmail.com>
2013-12-14 23:48:00 +0200

    DersEkle

---
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        public Form2 frm;
+        public Form1()
+        {
+            InitializeComponent();
+            frm = new Form2();
+            frm.frm1 = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frm.Show();
+        }
+    }
+}
