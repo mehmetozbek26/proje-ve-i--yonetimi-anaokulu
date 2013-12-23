c2615bdc57717f5493a582e690fbf0ad84967540
sedatakyol-49 <sedatakyol92@gmail.com>
2013-12-23 11:43:25 +0200

    GlobalClass

---
+++ AnaOkuluBilisim.son/AnaOkuluBilisim/GlobalClass (2).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnaOkuluBilisim
+{
+   public static class GlobalClass
+    {
+        private static string m_globalVar = "";
+        public static string GlobalVar
+        {
+            get { return m_globalVar; }
+            set { m_globalVar = value; }
+        }
+        private static string ogrid;
+        public static string Ogrid
+        {
+            get { return ogrid; }
+            set { ogrid = value; }
+        }
+       
+    }
+}
